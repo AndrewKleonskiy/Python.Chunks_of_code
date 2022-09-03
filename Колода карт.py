6c320c5508ad51acc24cd14054c2c59acecce669
Andrew Kleonskiy <co95310a@mail.ru>
2022-09-03 23:32:23 +0300

    Initial commit

---
+++ Колода карт.py
@@ -0,0 +1,34 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+beer_card = Card('7', 'diamonds')
+print(beer_card)
+deck = FrenchDeck()
+print(len(deck))
+
+from random import choice
+print(choice(deck))
+print(deck[:3])
+print(deck[12::13])
+for card in deck:
+    print(card)
+
+for card in reversed(deck):
+    print(card)
+
+print(Card('Q', 'hearts') in deck)
+print(Card('7', 'beasts') in deck)
